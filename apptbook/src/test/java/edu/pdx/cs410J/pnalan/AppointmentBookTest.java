9a83584a9bb564ec40af64d11313408d44934749
poojanalan10 <pnalan@pdx.edu>
2021-07-05 20:14:51 -0700

    Adding AppointmentBookTest class for unit test

---
+++ apptbook/src/test/java/edu/pdx/cs410J/pnalan/AppointmentBookTest.java
@@ -0,0 +1,27 @@
+package edu.pdx.cs410J.pnalan;
+
+import org.junit.jupiter.api.Test;
+import java.text.ParseException;
+
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class AppointmentBookTest {
+    @Test
+    public void checkIfGetOwnerNameReturnsOwnerName(){
+        AppointmentBook appBook = new AppointmentBook("Pooja", new Appointment());
+        assertThat(appBook.getOwnerName(),equalTo("Pooja"));
+    }
+    @Test
+    public void checkIfAppointmentBookAddsAppointments(){
+        AppointmentBook appBook = new AppointmentBook("Pooja", new Appointment("zoom meeting","02/02/2021","09:15","02/02/2021","11:00"));
+        assertThat(appBook.getAppointments().iterator().next().toString(),containsString("zoom meeting from 09:15 until 11:00"));
+    }
+
+    @Test
+    void toStringContainsAppointmentInTheAppointmentBook(){
+        AppointmentBook appBook = new AppointmentBook("Pooja", new Appointment("zoom meeting","02/02/2021","09:15","02/02/2021","11:00"));
+        assertThat(appBook.toString(),containsString("Pooja's appointment book with 1 appointment"));
+    }
+}
